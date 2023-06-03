dfd256aabce2c044e74d7ed73dbe7ed48a4d74ba
guntekhunter <guntek2000@gmail.com>
2023-06-03 19:34:50 +0800

    try to deploy again

--- routes/users.js
+++ routes/users.js
@@ -4,11 +4,7 @@ const UserController = require("../controller/users");
 const Authentication = require("../middleware/authentication");
 
 router.get("/", Authentication.requiredToken, UserController.getAllUsers);
-router.post(
-  "/register",
-  Authentication.requiredToken,
-  UserController.createUser
-);
+router.post("/register", UserController.createUser);
 router.post("/login", UserController.login);
 router.post("/logout", Authentication.removeToken, UserController.logOut);
 router.delete("/:id", Authentication.requiredToken, UserController.removeUser);
